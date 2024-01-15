33aa77fc2e662b7c8913a3a75a1f82ac614e1879
Kyusung98 <sgs3898@gmail.com>
2024-01-15 17:35:39 +0900

    Solution_day25

---
+++ programmers/src/main/java/com/example/programmers/lv0/Solution_day25.java
@@ -0,0 +1,19 @@
+package com.example.programmers.lv0;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution_day25 {
+    //문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때,
+    //my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
+    public String solution(String my_string, int num1, int num2) {
+        List<String> tmp = new ArrayList<>(List.of(my_string.split("")));
+
+        Collections.swap(tmp, num1, num2);
+
+        String answer = String.join("", tmp);
+
+        return answer;
+    }
+}
