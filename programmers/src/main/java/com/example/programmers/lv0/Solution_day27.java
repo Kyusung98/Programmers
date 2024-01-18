29b90652f921977501fccb3f28a622f648b29ae5
Kyusung98 <sgs3898@gmail.com>
2024-01-18 21:03:15 +0900

    Solution_day27

---
+++ programmers/src/main/java/com/example/programmers/lv0/Solution_day27.java
@@ -0,0 +1,23 @@
+package com.example.programmers.lv0;
+
+public class Solution_day27 {
+    //정수 n이 매개변수로 주어질 때,
+    //n의 약수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+    public int[] solution(int n) {
+        int cnt = 0;
+        for(int i = 1; i <= n; i++){
+            if (n % i == 0){
+                cnt++;
+            }
+        }
+        int[] answer = new int[cnt];
+        int array = 0;
+        for (int i = 1; i <= n; i++){
+            if (n % i == 0){
+                answer[array] = i;
+                array++;
+            }
+        }
+        return answer;
+    }
+}
