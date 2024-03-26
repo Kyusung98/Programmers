1065b1ef5c05bd4b371cf8a9e2f66fc3204d7f85
Kyusung98 <sgs3898@gmail.com>
2024-03-26 22:23:01 +0900

    Solution49

---
+++ programmers/src/main/java/com/example/programmers/basic/day10/Solution49.java
@@ -0,0 +1,16 @@
+package com.example.programmers.basic.day10;
+
+public class Solution49 {
+    //문자열 my_string과 두 정수 m, c가 주어집니다.
+    //my_string을 한 줄에 m 글자씩 가로로 적었을 때 왼쪽부터 세로로 c번째 열에 적힌 글자들을 문자열로 return 하는 solution 함수를 작성해 주세요.
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        for (int i = 0; i < my_string.length(); i++){
+            if (i == c - 1){
+                answer += my_string.charAt(i);
+                c += m;
+            }
+        }
+        return answer;
+    }
+}
