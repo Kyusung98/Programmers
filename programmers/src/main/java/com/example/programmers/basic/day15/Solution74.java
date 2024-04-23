62e4da9deb4be01f5b0fc369c473ac9ecc37b0f1
Kyusung98 <sgs3898@gmail.com>
2024-04-23 21:15:07 +0900

    Solution74

---
+++ programmers/src/main/java/com/example/programmers/basic/day15/Solution74.java
@@ -0,0 +1,16 @@
+package com.example.programmers.basic.day15;
+
+public class Solution74 {
+    //정수가 담긴 리스트 num_list가 주어질 때,
+    //리스트의 길이가 11 이상이면 리스트에 있는 모든 원소의 합을 10 이하이면 모든 원소의 곱을 return하도록 solution 함수를 완성해주세요.
+    public int solution(int[] num_list) {
+        int answer = 0;
+        if (num_list.length >= 11){
+            for (int num : num_list) answer += num;
+        }else {
+            answer = 1;
+            for (int num : num_list) answer *= num;
+        }
+        return answer;
+    }
+}
