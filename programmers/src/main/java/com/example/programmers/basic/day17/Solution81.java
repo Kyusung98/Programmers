375c385694351446b3e58e0c685c6c49cbfe541d
Kyusung98 <sgs3898@gmail.com>
2024-05-02 21:57:49 +0900

    Solution81

---
+++ programmers/src/main/java/com/example/programmers/basic/day17/Solution81.java
@@ -0,0 +1,9 @@
+package com.example.programmers.basic.day17;
+
+public class Solution81 {
+    //문자열 myString과 pat가 주어집니다.
+    //myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열을 찾아서 return 하는 solution 함수를 완성해 주세요.
+    public String solution(String myString, String pat) {
+        return myString.substring(0, myString.lastIndexOf(pat)) + pat;
+    }
+}
