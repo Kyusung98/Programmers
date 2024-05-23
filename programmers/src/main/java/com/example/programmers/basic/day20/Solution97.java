4a4e449194419d42e600038366e5864d02f7d7be
Kyusung98 <sgs3898@gmail.com>
2024-05-23 22:37:12 +0900

    Solution97

---
+++ programmers/src/main/java/com/example/programmers/basic/day20/Solution97.java
@@ -0,0 +1,34 @@
+package com.example.programmers.basic.day20;
+
+public class Solution97 {
+    //이 문제에서 두 정수 배열의 대소관계를 다음과 같이 정의합니다.
+    //두 배열의 길이가 다르다면, 배열의 길이가 긴 쪽이 더 큽니다.
+    //배열의 길이가 같다면 각 배열에 있는 모든 원소의 합을 비교하여 다르다면 더 큰 쪽이 크고, 같다면 같습니다.
+    //두 정수 배열 arr1과 arr2가 주어질 때, 위에서 정의한 배열의 대소관계에 대하여 arr2가 크다면 -1, arr1이 크다면 1, 두 배열이 같다면 0을 return 하는 solution 함수를 작성해 주세요.
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        int arr1Sum = 0;
+        int arr2Sum = 0;
+        if (arr1.length == arr2.length){
+            for (int i : arr1){
+                arr1Sum += i;
+            }
+            for (int j : arr2){
+                arr2Sum += j;
+            }
+            if (arr1Sum == arr2Sum){
+                answer = 0;
+            } else if (arr1Sum > arr2Sum) {
+                answer = 1;
+            } else {
+              answer = -1;
+            }
+        } else if (arr1.length > arr2.length){
+            answer =  1;
+
+        } else {
+            answer = -1;
+        }
+        return answer;
+    }
+}
