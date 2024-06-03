0c366cc2fef7211d3c72a82571977673e0679296
Kyusung98 <sgs3898@gmail.com>
2024-06-03 20:59:32 +0900

    Solution105

---
+++ programmers/src/main/java/com/example/programmers/basic/day21/Solution105.java
@@ -0,0 +1,10 @@
+package com.example.programmers.basic.day21;
+
+public class Solution105 {
+    //숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+    public int solution(String n_str) {
+        int answer = 0;
+
+        return Integer.parseInt(n_str);
+    }
+}
