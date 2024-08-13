62bbe76ec6428c6515183e05636f4290f1181244
Kyusung98 <sgs3898@gmail.com>
2024-08-13 22:41:06 +0900

    Solution21

---
+++ programmers/src/main/java/com/example/programmers/lv1/Solution21.java
@@ -0,0 +1,16 @@
+package com.example.programmers.lv1;
+
+public class Solution21 {
+    //단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요.
+    //단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+    //재한사항
+    //s는 길이가 1 이상, 100이하인 스트링입니다.
+    public String solution(String s) {
+        int len = s.length() / 2;
+        String[] arr = s.split("");
+        if (s.length() % 2 == 0){
+            return arr[len - 1] + arr[len];
+        }
+        return arr[len];
+    }
+}
